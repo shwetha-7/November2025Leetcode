e920a3e42f30a247137807555693ef627a762d5c
Shwetha K <shweetha.sweety17@gmail.com>
2025-11-04 00:32:07 +0530

    25-11-03 :: 2025 Nov 03 : Shwetha K : Minimum Time to Make Rope Colorful - Optimised Approach with constant space

--- MinimumTimetoMakeRopeColorful/MinimumTimetoMakeRopeColorful.py
+++ MinimumTimetoMakeRopeColorful/MinimumTimetoMakeRopeColorful.py
@@ -1,5 +1,6 @@
 class Solution:
     def minCost(self, colors: str, neededTime: list[int]) -> int:
+        
         max_value,n=0,len(colors)
         stack=[[colors[0],neededTime[0]]]
         for i in range(1,n):
@@ -13,7 +14,23 @@ class Solution:
             else:
                 stack.append([colors[i],neededTime[i]])
         return max_value
-            
+
+class Solution:
+    def minCost(self, colors: str, neededTime: list[int]) -> int:
+        max_value,sum_total=0,neededTime[0]
+        curr_char,char_value,n=colors[0],neededTime[0],len(colors)
+        for i in range(1,n):
+            if curr_char!=colors[i]:
+                max_value+=char_value
+                char_value=neededTime[i]
+                curr_char=colors[i]
+            else:
+                char_value=max(char_value,neededTime[i])
+            sum_total+=neededTime[i]
+        max_value+=char_value
+        return sum_total-max_value
+
+           
 class TestCase:
     def test_case_one(self):
         assert Solution().minCost("abaac",[1,2,3,4,5])==3
